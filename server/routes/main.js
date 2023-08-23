502b459f0d154019dc778ccfce74d70d8b834fbb
Roy Neville <royneville@Roys-MBP.attlocal.net>
2023-08-23 12:21:25 -0700

    Finish search functionality. Add bg img

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-//Routes
+//ROUTES
+
+//GET / Home
 router.get('', async (req, res) => {
   try {
     const locals = {
@@ -26,12 +28,73 @@ router.get('', async (req, res) => {
     const hasNextPage =
       nextPage <= Math.ceil(count / perPage);
 
-    // const data = await Post.find();
     res.render('index', {
       locals,
       data,
       current: page,
       nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//GET / Post: id
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description:
+        'Simple Blog created with NodeJs, Express & MongoDb.',
+    };
+
+    res.render('post', {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//POST / Post - searchTerm
+
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: 'Search',
+      description:
+        'Simple Blog created with NodeJs, Express & MongoDB',
+    };
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(
+      /[^a-zA-Z0-9]/g,
+      ''
+    );
+    const data = await Post.find({
+      $or: [
+        {
+          title: {
+            $regex: new RegExp(searchNoSpecialChar, 'i'),
+          },
+        },
+        {
+          body: {
+            $regex: new RegExp(searchNoSpecialChar, 'i'),
+          },
+        },
+      ],
+    });
+
+    res.render('search', {
+      data,
+      locals,
     });
   } catch (error) {
     console.log(error);
